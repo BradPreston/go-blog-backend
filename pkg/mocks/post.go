38ca3a8647c979501b8737b58933e7bcc45ff891
bradpreston <bap5393@gmail.com>
2022-08-02 19:51:21 -0400

    created a post model and a mock post for dev

--- pkg/mocks/post.go
+++ pkg/mocks/post.go
@@ -1,1 +1,19 @@
 package mocks
+
+import (
+	"time"
+
+	"github.com/BradPreston/go-blog-backend/pkg/models"
+)
+
+var Posts = []models.Post{
+	{
+		ID:        1,
+		Title:     "My first post",
+		Image:     "test.png",
+		Alt:       "missing image data",
+		Body:      "This is my very first blog post",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	},
+}
--- pkg/models/post.go
+++ pkg/models/post.go
@@ -1,1 +1,13 @@
 package models
+
+import "time"
+
+type Post struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Image     string    `json:"image"`
+	Alt       string    `json:"alt"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
