6335ea2b1c80cb8f2b1ea680934f2cd1e6ceb955
bradpreston <bap5393@gmail.com>
2022-08-02 19:43:29 -0400

    setup file structure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var port string = ":8080"
+
 func main() {
-	
+	router := mux.NewRouter()
+
+	log.Println("API is running on port:", port)
+	http.ListenAndServe(port, router)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/BradPreston/go-blog
+module github.com/BradPreston/go-blog-backend
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
