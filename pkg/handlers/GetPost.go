b7331f96e9a836787f2474936e9b58bad820d45c
bradpreston <bap5393@gmail.com>
2022-08-02 20:07:28 -0400

    added a GetPost method that gets one post from the mock data

--- pkg/handlers/GetPost.go
+++ pkg/handlers/GetPost.go
@@ -1,1 +1,25 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/BradPreston/go-blog-backend/pkg/mocks"
+	"github.com/gorilla/mux"
+)
+
+func GetPost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	for _, post := range mocks.Posts {
+		if post.ID == id {
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+
+			json.NewEncoder(w).Encode(post)
+			break
+		}
+	}
+}
